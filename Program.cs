8b1ed4941f48174dee5a1fab6125ec691259952c
Regis Magalhães <regis.silva3@senaisp.edu.br>
2022-01-23 23:01:36 -0300

    Algoritmo de soma feito em C#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Crie uma calculadora que faça a soma de 2 números inteiros");
+            //Entrada
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int soma = num1+num2;
+            //Saída
+            Console.Write("O resultado de " +num1+ " + " +num2+ " é " +soma);
+        }
+    }
+}
